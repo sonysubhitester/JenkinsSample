9c2fc62594d27c42cadbe428de7f855ea8f36eef
sonysubhitester <sonysubhisony@gmail.com>
2024-09-15 13:44:40 +0530

    Create Sample.java
    Initial commit of sample jenkins project

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class HelloGuys{
+  public static void main(String[] args){
+    System.out.println("january");
+    System.out.println("february");
+    System.out.println("march");
+    System.out.println("April");
+
+  }
+}
